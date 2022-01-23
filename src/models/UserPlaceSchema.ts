c343c2f1fca184542bd699fddba5b96f901c97d2
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-23 21:13:36 +0000

    feat-schema: create database model for vaccine

---
+++ src/models/UserPlaceSchema.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import { schemaOptions } from '@/utils/schemaOptions';
+
+const Schema = mongoose.Schema;
+
+const userPlaceSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    place: {
+      type: Schema.Types.ObjectId,
+      ref: 'Place',
+      required: true,
+    },
+  },
+  schemaOptions
+);
+
+export default mongoose.model('UserPlace', userPlaceSchema);
