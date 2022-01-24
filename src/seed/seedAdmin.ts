a08c579970e3abbfe9a471ca3d97cf1fa0e306ae
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-24 14:56:34 +0000

    featadmin: seeding the database with a default admin

---
+++ src/seed/seedAdmin.ts
@@ -0,0 +1,32 @@
+import CryptoJS from 'crypto-js';
+import AdminSchema from '@/models/AdminSchema';
+
+export const createAdmin = async (): Promise<any> => {
+  const username = process.env.DEFAULT_ADMIN_USERNAME!;
+  const password = process.env.DEFAULT_ADMIN_PASSWORD!;
+
+  try {
+    const admin = await AdminSchema.findOne({ username: username });
+
+    if (admin !== null) {
+      return true;
+    }
+
+    const newAdmin = new AdminSchema({
+      username,
+      password: CryptoJS.AES.encrypt(
+        password!,
+        process.env.PASSWORD_SECRET_KEY!
+      ),
+    });
+
+    console.log('--------------------------');
+    console.log('Admin created with');
+    console.log(`Username => ${username}`);
+    console.log(`Password => ${password}`);
+    console.log('--------------------------');
+    return await newAdmin.save();
+  } catch (error) {
+    console.error(error);
+  }
+};
