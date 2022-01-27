916f8d0b3abb5e4a6692fe39dfcc90d789157530
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-01-27 14:17:03 +0000

    feat-interface: creating interface (types) documents for controllers

--- src/interfaces/root.ts
+++ src/interfaces/root.ts
@@ -1,3 +1,5 @@
+import express from 'express';
+import mongoose from 'mongoose';
 export interface Controllers {
   create(): void;
   update(): void;
@@ -7,4 +9,62 @@ export interface Controllers {
 export interface AdminController {
   summary(): void;
   login(): void;
+}
+
+export interface TypedRequest<T> extends express.Request {
+  body: T;
+}
+
+export interface AdminDocument extends mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  username: string;
+  password: string;
+}
+
+export interface UserDocument extends mongoose.Document {
+  idNumber: string;
+  phoneNumber: string;
+  fullName: string;
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface PlaceDocument extends mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  address: string;
+  creator: UserDocument['_id'];
+}
+
+export interface UserPlaceDocument extends mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  user: UserDocument['_id'];
+  place: PlaceDocument['_id'];
+}
+
+export interface VaccineDocument extends mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+}
+
+export interface VaccineLOTDocument extends mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  quantity: number;
+  vaccinated: number;
+  vaccine: VaccineDocument['_id'];
+}
+
+export interface UserVaccineDocument extends mongoose.Document {
+  user: UserDocument['_id'];
+  vaccine: VaccineDocument['_id'];
+  vaccineLot: VaccineLOTDocument['_id'];
+  createdAt: Date;
+  updatedAt: Date;
 }
---
+++ src/models/schemas.ts
@@ -0,0 +1,9 @@
+import Admin from '@/models/admin.schema';
+import Place from '@/models/place.schema';
+import User from '@/models/user.schema';
+import UserPlace from '@/models/userPlace.schema';
+import UserVaccine from '@/models/userVaccine.schema';
+import Vaccine from '@/models/vaccine.schema';
+import VaccineLot from '@/models/vaccineLot.schema';
+
+export { Admin, Place, User, UserPlace, UserVaccine, Vaccine, VaccineLot };
