a45e1458f72b8b20750b0aec863378800827d12e
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-08-13 02:01:59 +0200

    Create an interface builder for place controller

--- src/interfaces/place.interface.ts
+++ src/interfaces/place.interface.ts
@@ -1,6 +1,35 @@
-import { Controllers } from './root';
+import express from 'express';
 
-export interface PlaceController extends Controllers {
-  getSinglePlace(): void;
-  getAllPlace(): void;
+import { Controllers, PlaceDocument, TypedRequest } from './root';
+
+export interface PlaceInterface extends Controllers {
+  getSinglePlace(req: express.Request, res: express.Response): void;
+  getAllPlace(req: express.Request, res: express.Response): void;
+}
+
+export abstract class PlaceInterfaceController implements PlaceInterface {
+  public abstract create(
+    req: TypedRequest<PlaceDocument>,
+    res: express.Response
+  ): Promise<void>;
+
+  public abstract delete(
+    req: TypedRequest<PlaceDocument>,
+    res: express.Response
+  ): Promise<void>;
+
+  public abstract getAllPlace(
+    req: TypedRequest<PlaceDocument>,
+    res: express.Response
+  ): Promise<void>;
+
+  public abstract getSinglePlace(
+    req: TypedRequest<PlaceDocument>,
+    res: express.Response
+  ): Promise<void>;
+
+  public abstract update(
+    req: TypedRequest<PlaceDocument>,
+    res: express.Response
+  ): Promise<void>;
 }
